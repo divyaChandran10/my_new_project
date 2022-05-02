b2af3096f2704c984f0f036d0de6e4ac0f35eb1d
divya chandran <divya.chandran10@gmail.com>
2022-05-02 12:19:11 +0200

    Added Task3

---
+++ May2_Exercises_Part1/Task3.py
@@ -0,0 +1,28 @@
+import datetime
+
+
+username = input("What is your username? ")
+password = input(f"Type the password for username {username}: ")
+valid = {"username": "admin", "password": "admin"}
+#today = datetime.date(2021, 8, 8)
+today = datetime.date(2021, 8, 7)
+#today = datetime.date(2021, 8, 9)
+
+# Your code here
+
+
+def is_weekend(date):
+    if (datetime.datetime.weekday(date)) == 5 or (datetime.datetime.weekday(date)) == 6:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(today.weekday())
+    if (username == valid.get('username') and password == valid.get('password')) or is_weekend(today):
+        print(f'Welcome, {username}!')
+    elif (username == valid.get('username') and password == valid.get('password')) and (is_weekend(today) == 'False'):
+        print(f'Welcome, {username}!')
+    else:
+        print('Credentials are invalid')
