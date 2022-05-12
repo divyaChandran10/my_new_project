57f47678ed628a3aacb84e83be45d88ea6785d71
divya chandran <divya.chandran10@gmail.com>
2022-05-12 16:00:57 +0200

    Updated with len condition

--- May12_Strings_in_Depth/Exercise3.py
+++ May12_Strings_in_Depth/Exercise3.py
@@ -2,12 +2,14 @@ if __name__ == '__main__':
     secret = input('*secret:*')
     name = input('*name:*')
     year = input('*year:*')
+    if len(secret) < 8:
+        print('Secret cannot be less than 8 characters')
+    else:
+        concat_string = secret + name
+        rev_concat_string = concat_string[::-1]
+        # First way
+        secret_code = rev_concat_string + year
+        print(secret_code)
 
-    concat_string = secret + name
-    rev_concat_string = concat_string[::-1]
-    # First way
-    secret_code = rev_concat_string + year
-    print(secret_code)
-
-    # second way
-    print(f'{rev_concat_string}{year}')
+        # second way
+        print(f'{rev_concat_string}{year}')
